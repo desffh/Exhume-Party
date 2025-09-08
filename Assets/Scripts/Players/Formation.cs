c5357d84dbe23365b3f1fc0ace17967e5dd6f45d
chikumo <yangju058@gmail.com>
2025-09-08 21:37:18 +0900

    (2025.09.08) 주석 추가

--- Assets/Scripts/Players/Formation.cs
+++ Assets/Scripts/Players/Formation.cs
@@ -37,7 +37,16 @@ public class Formation : MonoBehaviour
             row = rows.Count - 1;
 
         RowDefine define = rows[row]; // 배치될 행
+
+        /* index : 펫 번호
+         * consumed : 이전까지 몇마리의 해골이 배치되었는가 (누적합)
+         * (index - consumed) : 현재 줄에서 몇 번째 칸인가
+         * 
+         * Mathf.clamp(value, min, max) : value가 min보다 작으면 min반환, max보다 크면 max반환
+         * -> 범위를 고정하는 것.(min도 max도 아니라면 로직의 결과를 반환한다.)    */
         int col = Mathf.Clamp(index - consumed, 0, define.count - 1); // 배치될 칸(열)
+
+        /* (define.count - 1) * 0.5f : 이 행의 가로 폭 절반 */
 
         // X: 가운데 정렬 (줄 개수에 따라 좌우로 벌어지게)
         float xStart = -((define.count - 1) * 0.5f) * XSpacing; // 배치 시작 위치 (왼쪽 끝)
--- Assets/Scripts/Players/Movement.cs
+++ Assets/Scripts/Players/Movement.cs
@@ -9,6 +9,10 @@ public class Movement
         _rd = rd;
     }
 
+    /* Move()
+     * 
+     * -rd.position : 현재 위치
+     *    direction : 나아갈 방향  */
     public void Move(Vector3 direction, float moveSpeed)
     {
         _rd.MovePosition(_rd.position + direction * moveSpeed * Time.deltaTime);
